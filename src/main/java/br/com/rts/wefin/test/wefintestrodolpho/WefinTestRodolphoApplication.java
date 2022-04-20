7e7d113ad0853cb1f8476b635a465c93eae7a72e
rt.sotolani@gmail.com <Es@01062015>
2022-04-20 11:43:25 -0400

    Atualizado Classe application

---
+++ src/main/java/br/com/rts/wefin/test/wefintestrodolpho/WefinTestRodolphoApplication.java
@@ -0,0 +1,17 @@
+package br.com.rts.wefin.test.wefintestrodolpho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSpringDataWebSupport
+public class WefinTestRodolphoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WefinTestRodolphoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/rts/wefin/test/wefintestrodolpho/WefinTestRodolphoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rts.wefin.test.wefintestrodolpho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WefinTestRodolphoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
